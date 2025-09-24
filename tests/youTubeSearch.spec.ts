2c23229f7523e2ec4d5d4f4cc761c83fa093b031
Nocnica Mellifera <noctnica@gmail.com>
2025-09-24 15:05:42 -0700

    more tests

---
+++ tests/youTubeSearch.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.youtube.com/');
+  await page.getByRole('combobox', { name: 'Search' }).click();
+  await page.getByRole('combobox').pressSequentially('bowser nintendo ceo', {delay:1000});
+});
